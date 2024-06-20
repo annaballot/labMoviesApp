5599b8d1cf758937c887fa0a39a5958b43bdc643
Anna Ballot <annaballot@gmail.com>
2024-06-20 13:46:04 +0100

    Show movie review excerpts; Refactor Details page.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -55,3 +55,9 @@ export interface BaseMovieProps {
   export interface MovieListPageTemplateProps extends BaseMovieListProps {
     title: string;
   }
+
+  export interface Review{
+    id: string;
+    content: string
+    author: string
+  }
